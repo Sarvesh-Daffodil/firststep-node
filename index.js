5b476ff0755c6892376bc31315ab94a428688010
Sarvesh Singh <sarvesh.singh@daffodilsw.com>
2021-02-28 23:34:38 +0530

    Port Env Change

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 const express = require('express');
 const app = express();
 
-app.set('host', process.env.SERVER_HOST || '127.0.0.1');
-app.set('port', process.env.SERVER_PORT || 8090);
+app.set('HOST', process.env.SERVER_HOST || '127.0.0.1');
+app.set('PORT', process.env.SERVER_PORT || 5000);
 
 require('./src/boot')(__dirname, app)
     .then(() => {
-        app.listen(app.get('port'), () => {
+        app.listen(app.get('PORT'), () => {
             console.log('%s 🌎 is running at http://%s:%s in %s mode', '✓', app.get('host'), app.get('port'), app.get('env'));
         });
     })
